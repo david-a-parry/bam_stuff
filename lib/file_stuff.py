cd39a0e0b98fc1aa46bfce5bf4de19ac81f61838
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-07-11 12:39:30 +0100

    add seek_back_til_reads function

--- lib/file_stuff.py
+++ lib/file_stuff.py
@@ -1,3 +1,4 @@
+import sys
 import pysam
 
 def get_bamfile(bam):
@@ -17,4 +18,21 @@ def get_output(output, template):
             wbmode = 'wc'
         return pysam.AlignmentFile(output, wbmode, template=template)
     return  pysam.AlignmentFile('-', "w", template=template)
+
+def seek_back_til_reads(bamfile, start_tid=None, ref=None):
+    if start_tid is None:
+        start_tid = bamfile.nreferences - 1
+    sys.stderr.write("Seeking backwards through contigs until we find reads.\n")
+    got_one = 0
+    for i in range(start_tid, -1, -1):
+        sys.stderr.write("Trying contig {}\n"
+                         .format(bamfile.get_reference_name(i)))
+        for read in bamfile.fetch(tid=i, reference=ref):
+            if not got_one:
+                sys.stderr.write("At contig {}\n"
+                                 .format(bamfile.get_reference_name(i)))
+            got_one += 1
+        if got_one:
+            return
+        
 
